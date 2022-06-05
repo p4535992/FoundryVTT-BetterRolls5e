4ae6af2551ecd77d7c51371fed9aff0ab7bfc8d2
p4535992 <tentimarco0@gmail.com>
2022-06-05 21:19:50 +0200

    beta

---
+++ src/module/lib/lib.ts
@@ -0,0 +1,232 @@
+import CONSTANTS from '../constants';
+
+// =============================
+// Module Generic function
+// =============================
+
+export async function getToken(documentUuid) {
+  const document = await fromUuid(documentUuid);
+  //@ts-ignore
+  return document?.token ?? document;
+}
+
+export function getOwnedTokens(priorityToControlledIfGM: boolean): Token[] {
+  const gm = game.user?.isGM;
+  if (gm) {
+    if (priorityToControlledIfGM) {
+      const arr = <Token[]>canvas.tokens?.controlled;
+      if (arr && arr.length > 0) {
+        return arr;
+      } else {
+        return <Token[]>canvas.tokens?.placeables;
+      }
+    } else {
+      return <Token[]>canvas.tokens?.placeables;
+    }
+  }
+  if (priorityToControlledIfGM) {
+    const arr = <Token[]>canvas.tokens?.controlled;
+    if (arr && arr.length > 0) {
+      return arr;
+    }
+  }
+  let ownedTokens = <Token[]>canvas.tokens?.placeables.filter((token) => token.isOwner && (!token.data.hidden || gm));
+  if (ownedTokens.length === 0 || !canvas.tokens?.controlled[0]) {
+    ownedTokens = <Token[]>canvas.tokens?.placeables.filter((token) => (token.observer || token.isOwner) && (!token.data.hidden || gm));
+  }
+  return ownedTokens;
+}
+
+export function is_UUID(inId) {
+  return typeof inId === 'string' && (inId.match(/\./g) || []).length && !inId.endsWith('.');
+}
+
+export function getUuid(target) {
+  // If it's an actor, get its TokenDocument
+  // If it's a token, get its Document
+  // If it's a TokenDocument, just use it
+  // Otherwise fail
+  const document = getDocument(target);
+  return document?.uuid ?? false;
+}
+
+export function getDocument(target) {
+  if (target instanceof foundry.abstract.Document) return target;
+  return target?.document;
+}
+
+export function is_real_number(inNumber) {
+  return !isNaN(inNumber) && typeof inNumber === 'number' && isFinite(inNumber);
+}
+
+export function isGMConnected() {
+  return !!Array.from(<Users>game.users).find((user) => user.isGM && user.active);
+}
+
+export function isGMConnectedAndSocketLibEnable() {
+  return isGMConnected() && !game.settings.get(CONSTANTS.MODULE_NAME, 'doNotUseSocketLibFeature');
+}
+
+export function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function isActiveGM(user) {
+  return user.active && user.isGM;
+}
+
+export function getActiveGMs() {
+  return game.users?.filter(isActiveGM);
+}
+
+export function isResponsibleGM() {
+  if (!game.user?.isGM) return false;
+  return !getActiveGMs()?.some((other) => other.data._id < <string>game.user?.data._id);
+}
+
+// ================================
+// Logger utility
+// ================================
+
+// export let debugEnabled = 0;
+// 0 = none, warnings = 1, debug = 2, all = 3
+
+export function debug(msg, args = '') {
+  if (game.settings.get(CONSTANTS.MODULE_NAME, 'debug')) {
+    console.log(`DEBUG | ${CONSTANTS.MODULE_NAME} | ${msg}`, args);
+  }
+  return msg;
+}
+
+export function log(message) {
+  message = `${CONSTANTS.MODULE_NAME} | ${message}`;
+  console.log(message.replace('<br>', '\n'));
+  return message;
+}
+
+export function notify(message) {
+  message = `${CONSTANTS.MODULE_NAME} | ${message}`;
+  ui.notifications?.notify(message);
+  console.log(message.replace('<br>', '\n'));
+  return message;
+}
+
+export function info(info, notify = false) {
+  info = `${CONSTANTS.MODULE_NAME} | ${info}`;
+  if (notify) ui.notifications?.info(info);
+  console.log(info.replace('<br>', '\n'));
+  return info;
+}
+
+export function warn(warning, notify = false) {
+  warning = `${CONSTANTS.MODULE_NAME} | ${warning}`;
+  if (notify) ui.notifications?.warn(warning);
+  console.warn(warning.replace('<br>', '\n'));
+  return warning;
+}
+
+export function error(error, notify = true) {
+  error = `${CONSTANTS.MODULE_NAME} | ${error}`;
+  if (notify) ui.notifications?.error(error);
+  return new Error(error.replace('<br>', '\n'));
+}
+
+export function timelog(message): void {
+  warn(Date.now(), message);
+}
+
+export const i18n = (key: string): string => {
+  return game.i18n.localize(key)?.trim();
+};
+
+export const i18nFormat = (key: string, data = {}): string => {
+  return game.i18n.format(key, data)?.trim();
+};
+
+// export const setDebugLevel = (debugText: string): void => {
+//   debugEnabled = { none: 0, warn: 1, debug: 2, all: 3 }[debugText] || 0;
+//   // 0 = none, warnings = 1, debug = 2, all = 3
+//   if (debugEnabled >= 3) CONFIG.debug.hooks = true;
+// };
+
+export function dialogWarning(message, icon = 'fas fa-exclamation-triangle') {
+  return `<p class="${CONSTANTS.MODULE_NAME}-dialog">
+        <i style="font-size:3rem;" class="${icon}"></i><br><br>
+        <strong style="font-size:1.2rem;">${CONSTANTS.MODULE_NAME}</strong>
+        <br><br>${message}
+    </p>`;
+}
+
+// =========================================================================================
+
+export function cleanUpString(stringToCleanUp: string) {
+  // regex expression to match all non-alphanumeric characters in string
+  const regex = /[^A-Za-z0-9]/g;
+  if (stringToCleanUp) {
+    return i18n(stringToCleanUp).replace(regex, '').toLowerCase();
+  } else {
+    return stringToCleanUp;
+  }
+}
+
+export function isStringEquals(stringToCheck1: string, stringToCheck2: string, startsWith = false): boolean {
+  if (stringToCheck1 && stringToCheck2) {
+    const s1 = cleanUpString(stringToCheck1) ?? '';
+    const s2 = cleanUpString(stringToCheck2) ?? '';
+    if (startsWith) {
+      return s1.startsWith(s2) || s2.startsWith(s1);
+    } else {
+      return s1 === s2;
+    }
+  } else {
+    return stringToCheck1 === stringToCheck2;
+  }
+}
+
+/**
+ * The duplicate function of foundry keep converting my stirng value to "0"
+ * i don't know why this methos is a brute force solution for avoid that problem
+ */
+export function duplicateExtended(obj: any): any {
+  try {
+    //@ts-ignore
+    if (structuredClone) {
+      //@ts-ignore
+      return structuredClone(obj);
+    } else {
+      // Shallow copy
+      // const newObject = jQuery.extend({}, oldObject);
+      // Deep copy
+      // const newObject = jQuery.extend(true, {}, oldObject);
+      return jQuery.extend(true, {}, obj);
+    }
+  } catch (e) {
+    return duplicate(obj);
+  }
+}
+
+// =========================================================================================
+
+// =============================
+// Module specific function
+// =============================
+
+/**
+ * TODO if i need to manage the roll for specific system usually is enough item.roll()
+ * @href https://github.com/itamarcu/roll-from-compendium/blob/master/scripts/roll-from-compendium.js
+ */
+export async function rollDependingOnSystem(item: Item) {
+  // if (game.system.id === 'pf2e') {
+  //   if (item.type === 'spell') {
+  //     return pf2eCastSpell(item, actor, dummyActor)
+  //   } else {
+  //     return pf2eItemToMessage(item)
+  //   }
+  // }
+  // if (game.system.id === 'dnd5e') {
+  //   const actorHasItem = !!actor.items.get(item.id)
+  //   return dnd5eRollItem(item, actor, actorHasItem)
+  // }
+  //@ts-ignore
+  return item.roll();
+}
